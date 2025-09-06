95fc51a85c3e61ca1ef92f87f3e4e823f6ee0e94
ITR090 <raghad.4309@gmail.com>
2025-09-06 14:01:21 +0300

    adding fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,25 +5,79 @@ import './App.css'
 function App() {
 
   const [file, setFile] = useState(null);
-  
+  const [selected, setSelected] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [feedback, setFeedback] = useState(null)
+  console.log(feedback)
   const handleUpload = async () => {
 
-    const formData = new FormData();
-    formData.append("image", file);
-    const backendUrl = process.env.NODE_ENV == 'development' 
-    ? 'http://localhost:8080' 
-    : '';
-    console.log(backendUrl)
-    await axios.post(`${backendUrl}/upload`, formData);
-    alert("Image uploaded and processing started!");
+    try {
+      setLoading(true);
+      const formData = new FormData();
+      formData.append("file", file);
+      console.log(formData)
+      const backendUrl = process.env.NODE_ENV == 'development' ? 'http://localhost:8080' : '';
+      console.log(backendUrl)
+      const response = await axios.post(`${backendUrl}/upload`, formData);
+      console.log(response)
+      if (response.status === 200) {
+        setFeedback({
+          type: "success",
+          message: response.data.message,
+        })
+        //alert(response.data.message);
+      }
+    } catch (error) {
+      if (error.status === 400) {
+        setFeedback({
+          type: "error",
+          message: error.message,
+        })
+       //alert(error.message)
+      }
+      if (error.status === 500) {
+        setFeedback({
+          type: "error",
+          message: error.message,
+        })
+        //alert(error.message)
+      }
+    } finally {
+      setLoading(false);
+    }
 
   };
 
   return (
-    <div>
-      <h2>Upload Image</h2>
-      <input type="file" onChange={e => setFile(e.target.files[0])} />
-      <button onClick={handleUpload}>Upload</button>
+    <div className='flex flex-col justify-center items-center content-center h-screen w-screen'>
+      <h1 className='mb-3'>Upload image or pdf file</h1>
+      <input type="file" onChange={e => setFile(e.target.files[0])} className='mb-3 file-input file-input-neutral inline-block' />
+      <div className='w-auto'>
+        <div className='flex gap-2 cursor-pointer mt-1'>
+          <input type="checkbox" checked={selected === "resize"} onChange={() => setSelected(selected === "resize" ? null : "resize")} className="checkbox" />
+          <label className='text-sm'>Resize</label>
+        </div>
+        <div className='flex gap-2 cursor-pointer mt-1'>
+          <input type="checkbox" checked={selected === "compress"} onChange={() => setSelected(selected === "compress" ? null : "compress")} className="checkbox" />
+          <label className='text-sm'>Compress</label>
+        </div>
+      </div>
+      {!loading && <button onClick={handleUpload} className='btn btn-neutral mt-3'>Upload</button>}
+      {loading && <button className="btn btn-neutral mt-3">
+        <span className="loading loading-spinner"></span>
+        Loading
+      </button>}
+
+      {feedback && (<div role="alert" className={`alert alert-${feedback.type} mt-5`}>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+        <span>{feedback.message}</span>
+      </div>)}
+
+
+    
+
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -9,52 +9,73 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-const p_id =process.env.PROJECT_ID
+// project id
+const p_id = process.env.PROJECT_ID
+// cloud storage bucket
+const BUCKET_NAME = process.env.BUCKET_NAME;
+// cloud storage topic name
+const TOPIC_NAME = process.env.TOPIC_NAME;
+
+// Creates a client storage
+const storage = new Storage();
+// Creates a client pubsup
+const pubsub = new PubSub({ projectId: p_id });
+
 
 const upload = multer({ storage: multer.memoryStorage() });
 
-const storage = new Storage();
-const pubsub = new PubSub({p_id});
+app.post('/upload', upload.single('file'), async (req, res) => {
 
-const BUCKET_NAME = process.env.BUCKET_NAME;
-const TOPIC_NAME = process.env.TOPIC_NAME;
 
-app.post('/upload', upload.single('image'), async (req, res) => {
+  try {
 
-  const blob = storage.bucket(BUCKET_NAME).file(req.file.originalname);
-  const blobStream = blob.createWriteStream({
-    resumable: false,
-    metadata: {
-      contentType: req.file.mimetype,
-    },
-  });
+    if (!req.file) {
+      return res.status(400).json({ message: 'No file uploaded' });
+    }
+    const blob = storage.bucket(BUCKET_NAME).file(req.file.originalname);
+    const blobStream = blob.createWriteStream({
+      resumable: false,
+      metadata: {
+        contentType: req.file.mimetype,
+      },
+    });
 
-  blobStream.on('error', err => {
-    console.error('Upload error:', err);
-    return res.status(500).json({ message: 'Upload failed' });
-  });
+    // if any error during streem 
+    blobStream.on('error', err => {
+      console.error('Upload error:', err);
+      return res.status(500).json({ message: 'Upload failed' });
+    });
 
-  blobStream.on('finish', async ()=>{
-    const messageData = {
+    // if no errors 
+    blobStream.on('finish', async () => {
+      const messageData = {
         filename: req.file.originalname,
         bucket: BUCKET_NAME,
         contentType: req.file.mimetype,
         action: 'resize',
-        targetSizes: ['100x100', '500x500'],
+        targetSizes: ['500x500'],
         timestamp: Date.now()
       };
 
+      console.log(messageData)
       try {
-        await pubsub.topic(TOPIC_NAME).publishMessage({json: messageData});
-          console.log('Message published to ', TOPIC_NAME)
-          res.json({ message: 'Upload complete and message published' });
+        await pubsub.topic(TOPIC_NAME).publishMessage({ json: messageData });
+        console.log('Message published to ', TOPIC_NAME)
+        res.status(200).json({ message: 'Upload complete and message published' });
       } catch (error) {
         console.error('Pub/Sub publish error:', error);
         res.status(500).json({ message: 'Upload succeeded, but Pub/Sub failed' });
-    }
-  })
+      }
+    })
 
-  blobStream.end(req.file.buffer);
+    blobStream.end(req.file.buffer);
+    req.file.buffer = null;
+
+  } catch (error) {
+    res.status(500).json({ message: 'Upload failed' });
+  }
+
 });
+
 
 app.listen(8080, () => console.log("Listening on port 8080"));
